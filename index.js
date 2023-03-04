64755e16db46c39146965a50729669fdd60288eb
Maxim Bogochas <bogochunas@gmail.com>
2023-03-04 17:38:56 +0200

    fix: set as module

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+import { getInput, setOutput, setFailed } from '@actions/core';
+import { getOctokit, context } from '@actions/github';
 
 try {
-    const token = core.getInput('github-token');
+    const token = getInput('github-token');
     const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(`The event payload: ${payload}`);
-    const octokit = github.getOctokit(token)
-    const owner = github.context.payload.repository.owner;
-    const repo = github.context.payload.repository.name;
+    const octokit = getOctokit(token)
+    const owner = context.payload.repository.owner;
+    const repo = context.payload.repository.name;
     console.log(`Fetching info for '${owner}/${repo}'`);
 
     const info = await octokit.request(`GET /repos/${owner}/${repo}/releases`, {
@@ -22,5 +22,6 @@ try {
 
     // core.setOutput("time", time);
 } catch (error) {
-    core.setFailed(error.message);
+    setFailed(error.message);
 }
+
