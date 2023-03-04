e27ea21c1b7fbbfb38e454f404894538a6ab2d11
Maxim Bogochas <bogochunas@gmail.com>
2023-03-04 17:54:41 +0200

    fix: fetch only the last release

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ import { getOctokit, context } from '@actions/github';
 
 try {
     const token = getInput('github-token');
-    const payload = JSON.stringify(context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+    // const payload = JSON.stringify(context.payload, undefined, 2)
+    // console.log(`The event payload: ${payload}`);
     const octokit = getOctokit(token)
     const owner = context.payload.repository.owner.name;
     const repo = context.payload.repository.name;
     console.log(`Fetching info for '${owner}/${repo}'`);
 
-    const info = await octokit.request(`GET /repos/${owner}/${repo}/releases`, {
+    const info = await octokit.request(`GET /repos/${owner}/${repo}/releases/latest`, {
         owner,
         repo,
         headers: {
@@ -18,9 +18,15 @@ try {
         }
     });
 
-    console.log('Info: ', JSON.stringify(info, null, 2));
+    if (info.status === 200) {
+        setOutput('tag', info.tag_name);
+        setOutput('body', info.body);
+        setOutput('author', info.author.login);
+        console.log('Info: ', JSON.stringify(info, null, 2));
 
-    // core.setOutput("time", time);
+    } else {
+        setFailed('Fetch error');
+    }
 } catch (error) {
     setFailed(error.message);
 }
