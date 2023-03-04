122bf7c3ca400707c9b05eab4718ea7d6196815a
Maxim Bogochas <bogochunas@gmail.com>
2023-03-04 17:27:56 +0200

    fix: query repo

--- index.js
+++ index.js
@@ -3,19 +3,22 @@ const github = require('@actions/github');
 
 try {
     const token = core.getInput('github-token');
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
     const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(`The event payload: ${payload}`);
     const octokit = github.getOctokit(token)
+    const owner = github.context.payload.repository.owner;
+    const repo = github.context.payload.repository.name;
+    console.log(`Fetching info for '${owner}/${repo}'`);
 
-    // await octokit.request('GET /repos/{owner}/{repo}/releases', {
-    //     owner: 'OWNER',
-    //     repo: 'REPO',
-    //     headers: {
-    //         'X-GitHub-Api-Version': '2022-11-28'
-    //     }
-    // })
+    await octokit.request(`GET /repos/${owner}/${repo}/releases`, {
+        owner,
+        repo,
+        headers: {
+            'X-GitHub-Api-Version': '2022-11-28'
+        }
+    })
+
+    // core.setOutput("time", time);
 } catch (error) {
     core.setFailed(error.message);
 }
